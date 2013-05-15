50543b42fc01de23b7751049a0cee6b9ed65b99d
Krozark <maxime.barbier1991@gmail.com>
2013-05-15 20:23:28 +0200

    add config

---
+++ Config.hpp
@@ -0,0 +1,31 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#include <string>
+
+#include <SFML/Graphics.hpp>
+
+#include <ResourceManager/ResourceManager.hpp>
+
+namespace cfg
+{
+    class Config
+    {
+        Config() = delete;
+        Config(const Config&) = delete;
+        Config& operator=(const Config&) = delete;
+
+        public:
+
+        static std::string tex_path;
+        static std::string map_path;
+        static rm::ResourceManager<std::string,sf::Texture> textureManager;
+    };
+};
+
+std::string cfg::Config::tex_path("./data/textures");
+std::string cfg::Config::map_path("./data/map");
+rm::ResourceManager<std::string,sf::Texture> textureManager;
+
+
+#endif
